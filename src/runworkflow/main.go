671bc4428f0f7ac2fc80e0c1dee3656e60282c0a
Colt McNealy <100447728+coltmcnealy-lh@users.noreply.github.com>
2023-08-30 15:51:59 -0700

    Change README operations order and improves printing

--- src/runworkflow/main.go
+++ src/runworkflow/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"context"
 	"lh-quickstart-go/src"
-	"log"
 
+	"github.com/littlehorse-enterprises/littlehorse/sdk-go/common"
 	"github.com/littlehorse-enterprises/littlehorse/sdk-go/common/model"
 )
 
 func main() {
 	_, client := src.LoadConfigAndClient()
 	name := "bill"
-	wfId, err := (*client).RunWf(
+
+	common.PrintResp((*client).RunWf(
 		context.Background(),
 		&model.RunWfRequest{
 			WfSpecName: "my-workflow",
@@ -21,11 +22,5 @@ func main() {
 					Type: model.VariableType_STR,
 				},
 			},
-		})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Default().Println("got wfRunModel Id:", wfId)
-
+		}))
 }
